85158abffd70be6c8e2d29894e1a22f0c76e910f
Brant Merrell <brant.merrell@gmail.com>
2015-11-18 13:00:58 -0500

    tidying code example

--- explore.R
+++ explore.R
@@ -1,98 +1,75 @@
 rm(list=ls()) # clear workspace
 setwd("Josh/FDIC-Project")
 load('fdic.RData') # load FDIC functions
-fdicFolder<-"C:/Users/Kent/Documents/FDIC" # define folder for FDIC data
-download.FDIC(1993,3,fdicFolder) # download a quarter for testing
+zipFolder<-"C:/Users/Kent/Documents/Josh/Zips" # define folder for FDIC data
+tq<-c(year=1993, quarter=3) # choose a test quarter for testing
 
-# get the list of csv files to scan:
-fdicFiles<-list.files(fdicFolder,"199309.+\\.csv$",recursive = T, full.names = T)
+# select the corresponding zipFile
+zipFile<-list.files(zipFolder,pattern=quarterID(tq['year'],tq['quarter']),
+                    full.names = T)
+# unzip 
+unzip(zipFile, exdir = "./rawFiles")
 
-# define the variables to retrieve:
+# define the variables to assess:
 projectVars<-c("cert","asset","numemp","rbc1rwaj","RBCT2","rbcrwaj","RWAJT")
 
-# get all files with more than two of the project's variables (cert + one other):
+# remove all files without at least two of the project's variables (cert + one other):
+fdicFiles <- list.files("./rawFiles", 
+                        pattern = paste(quarterID(tq['year'],tq['quarter']),".+\\.csv$", sep = ""), 
+                        full.names = T)
 for(File in fdicFiles){
   ColNames<-colnames(read.csv(File,nrows = 3))
   ColNames<-ColNames[which(ColNames %in% projectVars)]
-  if(1<length(ColNames)){
-    ifelse(exists("projectFiles"), 
-           projectFiles<-c(projectFiles,File),
-           projectFiles<-File)
+  if(length(ColNames)<2){
+    file.remove(File)
   }
 }
-rm(fdicFiles) # clean workspace
+filePattern<-"\\d{8}_Assets and Liabilities.csv|\\d{8}_Performance and Condition Ratios.csv"
 
-for(File in projectFiles){
+zipFiles<-list.files(zipFolder,"All_Reports",full.names=T)
+for(ZIP in zipFiles){
+  # Files<-unzip(zipFiles[1],list=T)$Name
+  Files<-unzip(ZIP,list=T)$Name
+  Files<-Files[grepl(filePattern,Files)]
+  unzip(ZIP, files = Files, exdir = "./rawFiles")
+  print(Files)
+}
+dir.create("modified")
+File = list.files("rawFiles",pattern="\\.csv$",full.names = T)[1]
+for(File in list.files("rawFiles",pattern="\\.csv$",full.names = T)){
   DF<-read.csv(File)
-  projectColumns<-which(colnames(DF) %in% projectVars)
-  ifelse(exists("projectFrames"),
-         projectFrames<-list(projectFrames,DF[,projectColumns]),
-         projectFrames<-DF[,projectColumns])
-  
+  ColNames <- which(colnames(DF) %in% projectVars)
+  DF<-DF[,ColNames]
+  write.csv(DF,gsub("rawFiles","modified",File))
 }
-projectDF<-cbind(projectFrames[[1]],projectFrames[[2]])
-colnames(projectDF)
-head(projectDF[,c(1,5)])
-identical(projectDF[,1],projectDF[,5])
+for(File in list.files("rawFiles",pattern="\\.csv$",full.names = T)){
+  Message<-"This file has been cleared for space"
+  writeLines(Message,File)
+}
 
-DF<-projectQuarterFrame(1993,3)
-summary(DF)
-quarterID<-expand.grid(year=1994:2014,quar=1:4)
-for(n in 1:nrow(quarterID)){
-  DF<-rbind(DF,
-            projectQuarterFrame(quarterID[n,"year"],
-                                quarterID[n,"quar"]))
-  print(data.frame(Time=paste(Sys.time()),
-                   Year=quarterID[n,"year"],
-                   Q=quarterID[n,"quar"]))
-} # error after 1994.3
-save.image()
-file.remove(list.files(fdicFolder,full.names=T,recursive=T,pattern="\\.csv$"))
-rownames(DF)[seq(1,nrow(DF),length.out=20)]
-as.vector(DF[seq(1,nrow(DF),length.out=20),"quarter"])
-for(n in 44:nrow(quarterID)){
-  DF<-rbind(DF,
-            projectQuarterFrame(quarterID[n,"year"],
-                                quarterID[n,"quar"]))
-  print(data.frame(Time=paste(Sys.time()),
-                   Year=quarterID[n,"year"],
-                   Q=quarterID[n,"quar"]))
-} # error after 1994.3
-DF1<-DF
-rm(DF)
-write.csv(DF1,"data1.csv")
-rm(DF1)
+fdicFiles<-list.files("modified",full.names = T)
 
-DF<-projectQuarterFrame(quarterID[44,"year"],quarterID[44,"quar"])
-for(n in 44:nrow(quarterID)){
-  DF<-projectQuarterFrame(quarterID[n,"year"],quarterID[n,"quar"])
-  if(!ncol(DF) %in% c(7,8)){stop("new width of data frame")}
-  if(ncol(DF)==7){DF1<-rbind(DF1,DF)}
-  if(ncol(DF)==8){DF2<-rbind(DF2,DF)}
-  print(data.frame(Time=paste(Sys.time()),Year=quarterID[n,"year"],Q=quarterID[n,"quar"]))
-} # error immediate
-rownames(DF1)[seq(1,nrow(DF1),length.out=20)] # 1993.3 to 2001.2
-rownames(DF2)[seq(1,nrow(DF2),length.out=20)] # 2002.1 to 2009.2
-rownames(DF)[seq(1,nrow(DF),length.out=20)] # 2009.2
-head(DF1)
+DF1<-read.csv(fdicFiles[1],row.names="cert")
+DF1<-DF1[,-grep("^X$",colnames(DF1))]
+DF2<-read.csv(fdicFiles[2],row.names="cert")
+DF2<-DF2[,-grep("^X$",colnames(DF2))]
+
+DF<-data.frame(matrix(nrow=nrow(DF1),ncol=ncol(DF1)+ncol(DF2)),
+               row.names=row.names(DF1))
+colnames(DF)<-c(colnames(DF1),colnames(DF2))
+for(cert in rownames(DF)){
+  DF[cert,colnames(DF1)]<-DF1[cert,colnames(DF1)]
+  DF[cert,colnames(DF2)]<-DF2[cert,colnames(DF2)]
+  if(cert %in% rownames(DF)[seq(1,nrow(DF),length.out = 20)]){
+    print(DF[cert,])
+  }
+}
+
+
+discrepancies<-(which(rownames(DF1)!=rownames(DF2)))
+bindTest1<-DF1[head(discrepancies),]
+bindTest2<-DF2[head(discrepancies),]
+cbind(bindTest1,bindTest2)
+DF<-cbind(DF1,DF2)
+head(DF2[,-grep("^cert$",colnames(DF))])
 head(DF2)
-head(DF)
-for(n in 38:nrow(quarterID)){
-  DF<-projectQuarterFrame(quarterID[n,"year"],quarterID[n,"quar"])
-  if(!ncol(DF) %in% c(7,8)){stop("new width of data frame")}
-  if(ncol(DF)==7){DF1<-rbind(DF1,DF)}
-  if(ncol(DF)==8){DF2<-rbind(DF2,DF)}
-  print(data.frame(Time=paste(Sys.time()),Year=quarterID[n,"year"],Q=quarterID[n,"quar"]))
-}
-rownames(DF1)[seq(1,nrow(DF1),length.out=20)] # 1993.1 to 2001.3
-rownames(DF2)[seq(1,nrow(DF2),length.out=20)] # 2002.1 to 2014.4
-allQuarters<-unique(as.character(DF1$quarter))
-allQuarters<-c(allQuarters,unique(as.character(DF2$quarter)))
-allQuarters<-sort(allQuarters)
-sum(grepl(pattern=1994,allQuarters))
-quarterCount<-function(Pattern){sum(grepl(Pattern,allQuarters))}
-Counts<-lapply(1993:2014,quarterCount)
-unlist(Counts)
-1+length(1994:2014)*4
-file.remove(list.files(fdicFolder,full.names = T,recursive = T))
-DF<-projectQuarterFrame(2014,1)
